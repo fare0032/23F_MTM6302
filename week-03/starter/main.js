725c2657eb3c4f75ac72e9a270a3206d12efbf41
fare0032 <fare0032@algonquinlive.com>
2023-09-22 15:40:48 -0400

    adds html manipulation

--- week-03/starter/main.js
+++ week-03/starter/main.js
@@ -1,85 +1,95 @@
 /*** DOM ***/
 // Use the console to print the document title and the URL
-
+console.log( document.title )
 
 
 /*** Finding HTML Elements ***/
 //Select the element with the id = main. Store the returned value in a variable.
-
-
+const $main = document.getElementById('main')
+console.log( $main )
 
 //Select an element by CSS selector. Target the first link inside the 'nav' element.
-
+const $navFirstOption = document.querySelector('nav a')
+console.log($navFirstOption)
 
 
 //Target all links in the nav element
-
-
-
+const $navOptions = document.querySelectorAll('nav a')
+console.log($navOptions)
 
 //Pattern CSS selectors
 //select all elements in the document whose href attribute value starts with "https"
+console.log( document.querySelectorAll('[href^="https"]'))
 
 //select all elements in the document whose href attribute value contains the string "cdn"
-
+console.log( document.querySelectorAll('[href*="cdn"]'))
 
 /*** Traversing the DOM  ***/
 //Return a HTMLCollection with the child elements of the main node
+console.log( $main.children )
 // Returns the first child node(that is an element node) of main
+console.log( $main.firstElementChild )
 // Returns the next sibling element of main
-
+console.log( $main.nextElementSibling )
 
 
 /*** Manipulating attributes */
 //getAttribute()
 //Obtain the href attribute of $navFirstOption
-
+console.log( $navFirstOption.getAttribute('href') )
+console.log( $navFirstOption.href )
 
 
 //exception calling attributes as properties
-// console.log( $main.className )
-// console.log( $main.style);
-// console.log( $main.getAttribute('style'));
+console.log( $main.getAttribute('class') )
+console.log( $main.className )
+console.log( $main.style);
+console.log( $main.getAttribute('style'));
 
 //setAttribute()
 //Set the ID 'hero' on the first 'div' element inside the 'main' element.
-
+$main.firstElementChild.setAttribute('id','hero')
 
 //Add 'text-bg-dark' class to the element with id hero; warning!
-
+const $hero = document.getElementById('hero')
+// $hero.setAttribute('class','text-bg-dark')
 
 //Add the 'href' attribute to the 'continue reading...' hero link; https://imdac.github.io/mtm6302/
-
+const $heroCTA = document.querySelector('#hero a')
+$heroCTA.setAttribute('href','https://imdac.github.io/mtm6302/')
 
 //set the 'target' attribute of the her link to '_blank'
-
-// console.log($heroCTA.getAttribute('class')); // text-white fw-bol
-// console.log($heroCTA.getAttribute('href')); // https://imdac.github.io/mtm6302/
-// console.log($heroCTA.getAttribute('target')); // _blank
+$heroCTA.setAttribute('target','_blank')
+console.log($heroCTA.getAttribute('class')); // text-white fw-bol
+console.log($heroCTA.getAttribute('href')); // https://imdac.github.io/mtm6302/
+console.log($heroCTA.getAttribute('target')); // _blank
 
 //removeAttribute()
 //Remove the attribute 'target' from the 'continue reading...' link
+$heroCTA.removeAttribute('target')
 
 
 
 
 /*** Manipulating classes ***/
-// console.log(   $hero.classList)      // ["p-4 p-md-5 mb-4 rounded position-relative"]
+console.log(   $hero.classList)      // ["p-4 p-md-5 mb-4 rounded position-relative"]
 // Add class 'text-bg-dark' to the element with id 'hero'
+$hero.classList.add('text-bg-dark')
 
-// console.log($hero.classList)         // ["p-4 p-md-5 mb-4 rounded position-relative text-bg-dark"]
+console.log($hero.classList)         // ["p-4 p-md-5 mb-4 rounded position-relative text-bg-dark"]
 
 // Remove class
 // Remove the class 'text-bg-dark' from the element with id 'hero
+$hero.classList.remove('text-bg-dark')
 
-// console.log(element.classList)      // ["p-4 p-md-5 mb-4 rounded position-relative"]
+console.log($hero.classList)      // ["p-4 p-md-5 mb-4 rounded position-relative"]
 
 //Toggle class
 //Retreive the 'dark mode' button, and save it to $darkModeBtn const
-
+const $darkModeBtn = document.getElementById ('darkModeBtn')
 // $darkModeBtn.addEventListener('click', function() {
     // toggle class here
-
+$darkModeBtn.classList.toggle('text-bg-dark')
 // })
 
 
